fddd1a81ed75740d614dc8ed6e261ac8f21f9647
Oleksandr Galushka <bugblender@gmail.com>
2023-08-15 17:24:45 +0300

    Pieces ready

---
+++ CommandRunner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyapp
+{
+    public class CommandRunner
+    {
+        private readonly Process process;
+        public CommandRunner()
+        {
+            process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardInput = true;
+            startInfo.UseShellExecute = false;
+        }
+
+        public async Task<string> RunCommand(string command)
+        {
+            // TODO check if possible to pipe commands to single session cmd
+            var process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false;
+            startInfo.Arguments = $"/c {command}";
+            process.StartInfo = startInfo;
+            process.Start();
+            var output = await process.StandardOutput.ReadToEndAsync();
+            await process.WaitForExitAsync();
+
+            return output;
+        }
+    }
+}
